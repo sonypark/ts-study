55cbf973cf3eb15b75304b35580a2c0c165ce9f9
Sony <sonypark0204@gmail.com>
2019-09-28 20:14:44 +0900

    webpack 설정 수정
    - entry: 각 js 파일을 entry로 설정
    - output: 각 js 파일 별로 번들 파일이 생기도록 설정
    - devServer: webpack-dev-server와 관련된 옵션이다.contentBase는 serve할
    content를 가져올 경로를 가리킨다. dist 폴더로 설저해주었다

--- sonypark/webpack.config.js
+++ sonypark/webpack.config.js
@@ -2,7 +2,14 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.ts',
+  entry: {
+    add: './src/js/add.js',
+    book: './src/js/book.js',
+    edit: './src/js/edit.js',
+    index: './src/js/index.js',
+    login: './src/js/login.js',
+    profile: './src/js/profile.js',
+  },
   devtool: 'inline-source-map',
   module: {
     rules: [
@@ -21,13 +28,44 @@ module.exports = {
     extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
     new HtmlWebpackPlugin({
-      hash: true,
+      filename: 'index.html',
       template: './src/index.html',
+      chunks: ['index']
     }),
+    new HtmlWebpackPlugin({
+      filename: 'login.html',
+      template: './src/login.html',
+      chunks: ['login']
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'add.html',
+      template: './src/add.html',
+      chunks: ['add']
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'book.html',
+      template: './src/book.html',
+      chunks: ['book']
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'edit.html',
+      template: './src/edit.html',
+      chunks: ['edit']
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'profile.html',
+      template: './src/profile.html',
+      chunks: ['profile']
+    })
   ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8080
+  }
 };
